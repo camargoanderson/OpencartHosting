1bea084dbc913c132d1beaf5e8ccfc163e3f4570
Stanislav Derebcinschi <djbooom22@gmail.com>
2014-11-15 16:08:31 +0300

    -- Base Opencart v. 1.5.6

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencarthosting.local/admin/');
+define('HTTP_CATALOG', 'http://opencarthosting.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencarthosting.local/admin/');
+define('HTTPS_CATALOG', 'http://opencarthosting.local/');
+
+// DIR
+define('DIR_APPLICATION', 'G:\OpenServer\domains\opencarthosting.local/admin/');
+define('DIR_SYSTEM', 'G:\OpenServer\domains\opencarthosting.local/system/');
+define('DIR_DATABASE', 'G:\OpenServer\domains\opencarthosting.local/system/database/');
+define('DIR_LANGUAGE', 'G:\OpenServer\domains\opencarthosting.local/admin/language/');
+define('DIR_TEMPLATE', 'G:\OpenServer\domains\opencarthosting.local/admin/view/template/');
+define('DIR_CONFIG', 'G:\OpenServer\domains\opencarthosting.local/system/config/');
+define('DIR_IMAGE', 'G:\OpenServer\domains\opencarthosting.local/image/');
+define('DIR_CACHE', 'G:\OpenServer\domains\opencarthosting.local/system/cache/');
+define('DIR_DOWNLOAD', 'G:\OpenServer\domains\opencarthosting.local/download/');
+define('DIR_LOGS', 'G:\OpenServer\domains\opencarthosting.local/system/logs/');
+define('DIR_CATALOG', 'G:\OpenServer\domains\opencarthosting.local/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencarthosting');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencarthosting.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencarthosting.local/');
+
+// DIR
+define('DIR_APPLICATION', 'G:\OpenServer\domains\opencarthosting.local/catalog/');
+define('DIR_SYSTEM', 'G:\OpenServer\domains\opencarthosting.local/system/');
+define('DIR_DATABASE', 'G:\OpenServer\domains\opencarthosting.local/system/database/');
+define('DIR_LANGUAGE', 'G:\OpenServer\domains\opencarthosting.local/catalog/language/');
+define('DIR_TEMPLATE', 'G:\OpenServer\domains\opencarthosting.local/catalog/view/theme/');
+define('DIR_CONFIG', 'G:\OpenServer\domains\opencarthosting.local/system/config/');
+define('DIR_IMAGE', 'G:\OpenServer\domains\opencarthosting.local/image/');
+define('DIR_CACHE', 'G:\OpenServer\domains\opencarthosting.local/system/cache/');
+define('DIR_DOWNLOAD', 'G:\OpenServer\domains\opencarthosting.local/download/');
+define('DIR_LOGS', 'G:\OpenServer\domains\opencarthosting.local/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencarthosting');
+define('DB_PREFIX', 'oc_');
+?>
